02a87d02accaf90024dcee98c31221665e8ad8fd
Raj Patel <rajpatel8690@gmail.com>
2021-04-25 18:39:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import HomePage from './pages/homepage/homepage';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
