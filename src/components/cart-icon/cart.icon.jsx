dedf787b71e879f36c08e1b8f2ad083ecc0e10c8
Raj Patel <rajpatel8690@gmail.com>
2021-05-15 15:54:05 +0530

    updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart.icon.jsx
+++ src/components/cart-icon/cart.icon.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import {  } from '../../redux/cart/cart.action';
+import { toggleCartHidden } from '../../redux/cart/cart.action';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
@@ -18,4 +18,7 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default CartIcon;
+export default connect(
+    null,
+    mapDispatchToProps
+)(CartIcon);
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -9,7 +9,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown';
 
 import './header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to='/'>
             <Logo className='logo' />
@@ -29,12 +29,16 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon />
         </div>
-        <CartDropdown />
+        {
+            hidden ? null : <CartDropdown />
+        }
+        
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
